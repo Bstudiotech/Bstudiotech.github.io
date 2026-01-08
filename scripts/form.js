cc5ede5febfd114c5119f41426a2d1ebe69468a8
Idiol671 <sasha.yuferev@gmail.com>
2026-01-08 13:17:44 +0300

    Update form

--- scripts/form.js
+++ scripts/form.js
@@ -1,61 +1,61 @@
-emailjs.init('x0BsA4OZSEVg7pPR3')
+emailjs.init("vwA7cTp_4nJfw_Zce");
 
-const $modal = $('.form')
-const $content = $('.form-content')
-const $formBlock = $('.form-block').first()
-const $thanks = $('#formThanks')
-const $submit = $('.form-button--submit')
+const $modal = $(".form");
+const $content = $(".form-content");
+const $formBlock = $(".form-block").first();
+const $thanks = $("#formThanks");
+const $submit = $(".form-button--submit");
 
-let scrollY = 0
+let scrollY = 0;
 
 function setLoading(state) {
-	$submit.toggleClass('is-loading', state).prop('disabled', state)
+  $submit.toggleClass("is-loading", state).prop("disabled", state);
 }
 
 function openModal() {
-	$modal.addClass('is-open')
+  $modal.addClass("is-open");
 }
 
 function closeModal() {
-	$modal.removeClass('is-open')
-	setLoading(false)
-	$('#consultForm')[0].reset()
+  $modal.removeClass("is-open");
+  setLoading(false);
+  $("#consultForm")[0].reset();
 
-	setTimeout(() => {
-		$formBlock.removeClass('is-hidden')
-		$thanks.addClass('is-hidden')
-	}, 300)
+  setTimeout(() => {
+    $formBlock.removeClass("is-hidden");
+    $thanks.addClass("is-hidden");
+  }, 300);
 }
 
 function showThanks() {
-	$formBlock.addClass('is-hidden')
-	setTimeout(() => $thanks.removeClass('is-hidden'), 150)
+  $formBlock.addClass("is-hidden");
+  setTimeout(() => $thanks.removeClass("is-hidden"), 150);
 }
 
-$('.open-form-button').on('click', openModal)
-$('.close, .form-button--close').on('click', closeModal)
+$(".open-form-button").on("click", openModal);
+$(".close, .form-button--close").on("click", closeModal);
 
-$modal.on('click', e => {
-	if (!$(e.target).closest('.form-content').length) closeModal()
-})
+$modal.on("click", (e) => {
+  if (!$(e.target).closest(".form-content").length) closeModal();
+});
 
-$('#consultForm').on('submit', function (e) {
-	e.preventDefault()
-	setLoading(true)
+$("#consultForm").on("submit", function (e) {
+  e.preventDefault();
+  setLoading(true);
 
-	emailjs
-		.send('service_v6kpnud', 'template_vxp8xrr', {
-			name: $('[name="name"]').val(),
-			email: $('[name="email"]').val(),
-			subject: $('[name="subject"]').val(),
-			message: $('[name="message"]').val(),
-		})
-		.then(() => {
-			setLoading(false)
-			showThanks()
-		})
-		.catch(err => {
-			console.error(err)
-			setLoading(false)
-		})
-})
+  emailjs
+    .send("service_v6kpnud", "template_vxp8xrr", {
+      name: $('[name="name"]').val(),
+      email: $('[name="email"]').val(),
+      subject: $('[name="subject"]').val(),
+      message: $('[name="message"]').val(),
+    })
+    .then(() => {
+      setLoading(false);
+      showThanks();
+    })
+    .catch((err) => {
+      console.error(err);
+      setLoading(false);
+    });
+});
